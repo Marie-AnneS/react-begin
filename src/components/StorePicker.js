01de990cc3a220c6555f983b146fb60330521630
Maria Seim <marie.anne.seim@gmail.com>
2019-08-16 22:58:25 -0400

    take input from the form and put in the url

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,17 +2,24 @@ import React from "react";
 import { getFunName } from "../helpers";
 
 class StorePicker extends React.Component {
-handleClick(){
-  alert("Hey!");
-}
+  myInput = React.createRef();
 
+  goToStore = (event) => {
+  //1. stop the form submiting
+  event.preventDefault();
+  //2. get the text from that input
+  //console.log(this.myInput.current.value);  
+  const storeName = this.myInput.current.value;  
+  //3. change the page to /store/whatever-they-entered
+  this.props.history.push(`/store/${storeName}`);
+}
   render() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore}>
         <h2>Please Enter A Store</h2>
-        <button onClick={this.handleClick}>Click me!</button>
         <input
           type="text"
+          ref={this.myInput}
           required
           placeholder="Store Name"
           defaultValue={getFunName()}
