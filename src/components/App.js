ab07ff881e68f52aa739db99d3301fa4a73a782b
Maria Seim <marie.anne.seim@gmail.com>
2019-08-24 23:50:13 -0400

    finish the Fish component and show data

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import Header from "./Header";
 import Inventory from "./Inventory";
 import Order from "./Order";
+import sampleFishes from "../sample-fishes";
+import Fish from "./Fish";
 
 class App extends React.Component {
   //2 ways to have a state : 1 est de le mettre dans un constructeur
@@ -25,7 +27,7 @@ class App extends React.Component {
   };
 
   loadSampleFishes = () => {
-    alert("Loading Sample");
+    this.setState({ fishes: sampleFishes });
   };
 
   render() {
@@ -33,6 +35,11 @@ class App extends React.Component {
       <div className="catch-of-the-day">
         <div className="menu">
           <Header tagline="Fresh Seafood Market" />
+          <ul className="fishes">
+            {Object.keys(this.state.fishes).map(key => (
+              <Fish key={key} details={this.state.fishes[key]} />
+            ))}
+          </ul>
         </div>
         <Order />
         <Inventory
