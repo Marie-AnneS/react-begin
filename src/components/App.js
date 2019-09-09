50501a0baec0891fcddf253f6207dff454fee0b1
Maria Seim <marie.anne.seim@gmail.com>
2019-09-09 10:11:40 -0400

    adding th fonctionnality of the sold out BTN and add order in the state

--- src/components/App.js
+++ src/components/App.js
@@ -30,6 +30,15 @@ class App extends React.Component {
     this.setState({ fishes: sampleFishes });
   };
 
+  addToOrder = (key) => {
+    // 1. prendre une copies du state existant
+    const order = { ...this.state.order };
+    // 2.ajouter une commande ou update le nombre de commande
+    order[key] = order[key] + 1 || 1
+    // 3. call setState et update le state
+    this.setState({ order });
+  }
+
   render() {
     return (
       <div className="catch-of-the-day">
@@ -37,7 +46,7 @@ class App extends React.Component {
           <Header tagline="Fresh Seafood Market" />
           <ul className="fishes">
             {Object.keys(this.state.fishes).map(key => (
-              <Fish key={key} details={this.state.fishes[key]} />
+              <Fish key={key} index={key} details={this.state.fishes[key]} addToOrder={this.addToOrder} />
             ))}
           </ul>
         </div>
--- src/components/Fish.js
+++ src/components/Fish.js
@@ -2,10 +2,15 @@ import React, { Component } from "react";
 import { formatPrice } from "../helpers";
 
 export class Fish extends Component {
+  //function si elle est utilise moins d'une fois. mettre dans le BTN
+  /* handleClick = () => {
+    this.props.addToOrder(this.props.index);
+  } */
+
   render() {
     // deconstructering
     const { image, name, price, desc, status } = this.props.details;
-
+    const isAvailable = status === "available";
     return (
       <li className="menu-fish">
         <img src={image} alt={name} />
@@ -14,7 +19,14 @@ export class Fish extends Component {
           <span className="price">{formatPrice(price)}</span>
         </h3>
         <p>{desc}</p>
-        <button>Add To Cart</button>
+        <button
+          disabled={!isAvailable}
+          onClick={() => {
+            this.props.addToOrder(this.props.index);
+          }}
+        >
+          {isAvailable ? "Add To Order" : "Sold out!"}}
+        </button>
       </li>
     );
   }
