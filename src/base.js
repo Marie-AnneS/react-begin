4fa7218be2d211680657215c767372cd970c8b80
Maria Seim <marie.anne.seim@gmail.com>
2019-09-10 14:20:21 -0400

    hide my api key

--- src/base.js
+++ src/base.js
@@ -1,10 +1,11 @@
 import Rebase from "re-base";
 import firebase from "firebase";
 
+
 const firebaseApp = firebase.initializeApp({
-  apiKey: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
-  authDomain: "xxxxxxxxxxxxxxxxxxxxx",
-  databaseURL: "xxxxxxxxxxxxxxxxxxxxxx",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.AUTH_DOMAIN_FIREBASE,
+  databaseURL: "https://catch-of-the-day-seim.firebaseio.com",
 });
 
 const base = Rebase.createClass(firebaseApp.database());
