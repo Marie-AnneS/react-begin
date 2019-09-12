11c5d58db458fdffc679d10a85da0bed3134dee3
Maria Seim <marie.anne.seim@gmail.com>
2019-09-11 21:57:43 -0400

    Authentication Adding th facebook and github aut with firebase

--- src/base.js
+++ src/base.js
@@ -1,7 +1,7 @@
 import Rebase from "re-base";
 import firebase from "firebase";
 
-
+// ATTENTION Doit mettre le apiKey, authDomain en brut video 24 14:36
 const firebaseApp = firebase.initializeApp({
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.AUTH_DOMAIN_FIREBASE,
